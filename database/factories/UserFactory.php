5ac8445def28c12c859c4e5004d0b56246933497
hafifradhitya <bangclasher81@gmail.com>
2025-02-10 11:51:39 +0700

    membuat file migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->username(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('123123123'),
+            'name' => fake()->name(),
             'remember_token' => Str::random(10),
         ];
     }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('id_user');
+            $table->string('username', 255);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            $table->string('name', 255);
             $table->rememberToken();
             $table->timestamps();
         });
